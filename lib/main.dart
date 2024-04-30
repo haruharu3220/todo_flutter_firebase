f02657f5e38c1b83fb8abeb1d3d434ea73587d88
Hayato <hayato.314.nico@gmail.com>
2024-04-30 10:10:29 +0900

    データ更新用のメソッド追加(中身はまだ無い)　Future型は非同期

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,7 +45,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+
+  List<Item> items = [];
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    watch();
+  }
+
+  //データ更新監視
+  Future<void> watch() async {}
+
+  //保存する
+  Future<void> dave() async {}
+
+  //完了・未完了に変更する
+  Future<void> complete(Item item) async {}
+
+  //削除する
+  Future<void> delete(String id) async {}
+
 
   void _incrementCounter() {
     setState(() {
