f013beb5d9c510551a4595b9e237c409712c41a3
Hayato <hayato.314.nico@gmail.com>
2024-04-30 12:15:56 +0900

    UInoの作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -11,22 +11,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,7 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -45,7 +33,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<Item> items = [];
   final TextEditingController textEditingController = TextEditingController();
 
@@ -59,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> watch() async {}
 
   //保存する
-  Future<void> dave() async {}
+  Future<void> save() async {}
 
   //完了・未完了に変更する
   Future<void> complete(Item item) async {}
@@ -67,42 +54,34 @@ class _MyHomePageState extends State<MyHomePage> {
   //削除する
   Future<void> delete(String id) async {}
 
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Todo list'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return ListTile(
+              title: TextField(
+                controller: textEditingController,
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  save();
+                },
+                child: Text('保存'),
+              ),
+            );
+          }
+          final item = items[index - 1];
+          return ListTile(
+            title: Text(item.text),
+          );
+        },
+        itemCount: items.length + 1,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
